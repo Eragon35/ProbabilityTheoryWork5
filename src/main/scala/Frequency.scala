55a1a8de83e00564e4dd2b0db906e8a594526039
Eragon35 <antipin35@yahoo.com>
2021-05-07 18:45:34 +0300

    Remake freqincy histogram from func(0) to func(0) - l/2

--- src/main/scala/Frequency.scala
+++ src/main/scala/Frequency.scala
@@ -4,36 +4,29 @@ import scala.collection.mutable.ArrayBuffer
 
 object Frequency {
   def show(func: ArrayBuffer[Double]): Unit = {
-    val distinct = func.distinct
-    // полигон частот графиком
-    val dataset = Seq(
-      ("", for (x <- distinct) yield (x, func.groupBy(identity).mapValues(_.size)(x).toDouble / func.size)),
-      ("y = 0", for (x <- distinct) yield (x, 0.0))
-    )
-    XYLineChart(dataset.toXYSeriesCollection()).show("Полигон частот", (1280, 720), scrollable = true)
-
-
-    // высчитываю данные для гистограммы частот
+    // высчитываю данные для группированной выборки частот
     val k = (Math.log10(func.size)/Math.log10(2) + 1).toInt // k = 1+ log2 (20).round - число интервалов выборки
     val l = (func(func.size - 1) - func(0)) / k // длинна интервала
     var histogram: ArrayBuffer[(Double, Int)] = ArrayBuffer[(Double, Int)]() // (начало промежутка, число значений на нём)
-    for (i <- 0 until k) histogram = histogram :+ (func(0) + l * i,
-      func.count(_ < func(0) + l * (i+1) + 0.01) - histogram.map(z => z._2).sum)
-    histogram = histogram :+ (func(func.size - 1), histogram(k-1)._2)
-    // -1.73 to -1.044 = 2 => 0.1
-    // -1.044 to -0.358 = 2 => 0.1
-    // -0.358 to 0.329 = 8 => 0.4
-    // 0.329 to 1.014 = 4 => 0.2
-    // 1.014 to 1.7 = 4 => 0.2
+    for (i <- 0 to k) histogram = histogram :+ (func(0) + l * (i - 0.5),
+      func.count(_ < func(0) + l * (i+0.5) + 0.01) - histogram.map(z => z._2).sum)
+    histogram = histogram :+ (func(func.size - 1) + l / 2, histogram(k-1)._2) // второе значение не играет роли конкретно тут
+    println("Интервальный статистический ряд:")
+    for (i <- 0 to k) println(f"[${histogram(i)._1}%1.3f, ${histogram(i+1)._1}%1.3f) P(x) = ${histogram(i)._2.toDouble / func.size}")
+    var dataset = Seq[(Double, Double)]((histogram(0)._1, histogram(0)._2.toDouble / func.size))
+    for (i <- 1 until k) dataset = dataset :+ (histogram(i)._1, histogram(i)._2.toDouble / func.size)
+    dataset = dataset :+ (histogram(histogram.size - 1)._1, histogram(histogram.size - 1)._2.toDouble / func.size)
+    // полигон группированной выборки частот графиком
+    XYLineChart(Seq(("", dataset.toIndexedSeq)).toXYSeriesCollection()).show("Полигон частот", (1280, 720), scrollable = true)
 
 
-    // показываю гистограмму частот
-    var dataset4 = Seq[(String, Double)]()
-    for (i <- 0 until k) dataset4 = dataset4 :+ (
+    // показываю гистограмму группированной выборки частот
+    var dataset2 = Seq[(String, Double)]()
+    for (i <- 0 to k) dataset2 = dataset2 :+ (
       (s"${BigDecimal(histogram(i)._1).setScale(3, BigDecimal.RoundingMode.HALF_UP).toString}" + // имя столбца
       s" to ${BigDecimal(histogram(i+1)._1).setScale(3, BigDecimal.RoundingMode.HALF_UP).toString}") ->
         histogram(i)._2.toDouble / func.size // значение(высота) столбца
     )
-    BarChart(dataset4).show("Гистограмма частот", (1280, 720), scrollable = true)
+    BarChart(dataset2).show("Гистограмма частот", (1280, 720), scrollable = true)
   }
 }
